a77897e6092fffded34e6710c1213ca9e05545df
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-02-19 23:43:24 -0400

    feat: Use \n on while loop

--- Lessons/while.php
+++ Lessons/while.php
@@ -5,17 +5,17 @@ $attempts = 0;
 $max_attempts = 5;
 
 while($attempts < $max_attempts) {
- echo "What is the secret? ";
+ echo "What is the secret? \n";
  $input = trim(fgets(STDIN));
  $attempts++;
  if($input === $secret) {
-     echo "Congratulations! You know the secret! ";
+     echo "Congratulations! You know the secret! \n";
      break;
  } else {
-     echo "Nope, try again.";
+     echo "Nope, try again.\n";
  }
 
  if($attempts >= $max_attempts) {
-     echo "Too many wrong attempts. ";
+     echo "Too many wrong attempts. \n";
  }
 } 
