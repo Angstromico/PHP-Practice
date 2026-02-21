0f5a28532ca7bf3bc509d8ddba7907c88104f044
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-02-21 13:23:38 -0400

    practice: optional parameters in functions

--- Lessons/functions/functions.php
+++ Lessons/functions/functions.php
@@ -4,3 +4,20 @@ function greet(string $name) : string {
 }
 
 echo greet("Alice\n");
+
+function greetWithTime(string $name, ?DateTimeInterface $time = null): string {
+    $formatTime = $time ?? new DateTime();
+    
+    $hour = (int)$formatTime->format("H");
+
+    if ($hour < 12) {
+        return "Good morning, $name!\n";
+    } elseif ($hour < 18) {
+        return "Good afternoon, $name!\n";
+    } else {
+        return "Good evening, $name!\n";
+    }
+}
+
+echo greetWithTime("Memz"); // Current hour
+echo greetWithTime("Memz", new DateTime("20:00:00"));
